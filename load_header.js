ec55ce7ac1e5971b58d144a9d0ce5c441bb67eaf
joseph.cheng <joseph.cheng@bp.com>
2022-02-20 11:04:13 +1100

    reverted everything back into single page static page

---
+++ load_header.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function() {
+  // this function runs when the DOM is ready, i.e. when the document has been parsed
+  // const navLinks = document.querySelectorAll('.nav-item')
+  // const menuToggle = document.getElementById('navbarSupportedContent')
+  // const bsCollapse = new bootstrap.Collapse(menuToggle, {
+  //   toggle: false
+  // })
+  // navLinks.forEach((l) => {
+  //   l.addEventListener('click', () => {
+  //     bsCollapse.toggle()
+  //   })
+  // })
+
+  // Highlight navbar item of current page
+  // $(function () {
+  //   $('a').each(function () {
+  //     if ((window.location.pathname.indexOf($(this).attr('href'))) > -1) {
+  //       $(this).toggleClass("hovered");
+  //     }
+  //   });
+  // });
+
+});
