c73e80c82ccf2a58c1101881af9fc93171658286
Eyew0001 <32605278+Eyew0001@users.noreply.github.com>
2022-02-23 15:40:26 +1100

    show and hide div
    on click of images show and hide further text div

--- services.js
+++ services.js
@@ -1,5 +1,24 @@
-$(document).ready(function(){
-    $('.services-btn').click(function(){
-        $(this).parent().find(".services-list").slideToggle();
+$(document).ready(function () {
+    $('.services-btn').click(function () {
+        $(this).parent().find('.services-list').slideToggle();
     });
+
+    var number;
+    $('.project-highlights-img').click(function () {
+        number = $('.project-highlights-img').index(this);
+        if ($('.project-highlights').eq(number).css("display") == 'none') {
+            console.log('not open');
+            $('.project-highlights').hide();
+            $('.project-highlights').eq(number).slideToggle();
+            console.log('now open');
+        }
+        else {           
+            $('.project-highlights').eq(number).slideToggle();
+            console.log('now closed');
+        }
+        
+        
+    });
+
+
 });
